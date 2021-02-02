804e8071e32ec0cb4ea790a720f78f65d80fa446
Andrea <fortinia95@gmail.com>
2021-02-02 16:03:35 +0100

    completamento progetto

--- README.md
+++ README.md
@@ -1,22 +1,71 @@
 # C Simple Parser
 
-The project aims to implement simple parser for sub-language of C. The parser can recognize:
+The project aims to implement simple parser for sub-language of C with pattern-matching technique. The parser should recognize:
 * Expression
 * Assignement 
 * Int variable declaration
 * Selection
 * Block {}
-* Loop
+* Loops
 
-The parser produces an Abstract Syntax Tree (AST) as output.
+It produces an Abstract Syntax Tree (AST) as output.
+
+## Usage
+
+The program should be compile with ghc and the executable takes two argument: the first one is the input file (.txt) and the second is the output file where the program will print the AST.
+
+```bash
+$ ghc c-parser.hs
+$ ./c-parser input.txt output.txt
+```
+
+The input file shouldn't have main, but it should be wrapped between curly brackets.
+Check the input file 'input.txt' and try to run the program to see how it works.
+
 
 ## Grammar
 
-expression
-	: assignment_expression
-	| expression ',' assignment_expression
-	;
-assignment_expression
-	: conditional_expression
-	| unary_expression assignment_operator assignment_expression
-	;
+```EBNF
+<program> ::= <compound>
+
+<compound> ::= "{" <declaration_list> <statement_list> "}"
+
+<declaration_list> ::= {<declaration>}
+
+<declaration> ::= "int" string ";"
+
+<statement_list> ::= {<statement>}
+
+<statement> ::= <if_statement>
+ |	<while_statement>
+ |  <do_statement>
+ |  <for_statement>
+ |  <assignment_statement>
+ |  <compound>
+
+<if_statement> ::= "if" "(" <expression> ")" <statement> [<opt_else>]
+
+<opt_else> ::= "else" <statement>
+
+<while_statement> ::= "while" "(" <expression> ")" <statement>
+
+<do_statement> ::= "do" "{" <statement> "}" "while" "(" <expression> ")" ";"
+
+<for_statement> ::= "for" "(" <assignment_statement> ";" <expression> ";" <expression> ")" <statement>
+
+<assignment_statement> ::= string "=" <expression>
+
+<expression> ::= <equality>
+
+<equality> ::= <comparison> { ("!="|"==") <comparison> }
+
+<comparison> ::= <term> { (">"|">="|"<"|"<=") <term> }
+
+<term> ::= <factor> { ("-"|"+") <factor> }
+
+<factor> ::= <unary> { ("/"|"*") <unary> }
+
+<unary> ::= ("!"|"-") <unary> | <primary>
+
+<primary> ::= number | string | "(" <expression> ")"
+```
--- c-parser.hs
+++ c-parser.hs
@@ -2,237 +2,355 @@ import System.Environment
 import System.IO
 import Data.Char
 import Data.List
+import Data.List.Split
 
 data AST 
-  = Add     AST AST
-  | Sub     AST AST
-  | Mult    AST AST
-  | Div     AST AST
-  | Eq      AST AST
-  | Gt      AST AST
-  | Lt      AST AST
-  | Not     AST
-  | Assign  AST AST
-  | Decl    AST AST
-  | Seq     AST AST 
-  | Cond    AST AST
-  | Loop    AST AST
-  | Val     Int
-  | Id      String
-  | Type    String
+  = Block       [Declaration] [AST] 
+  | While       Expr     AST 
+  | DoWhile     AST      Expr 
+  | If          Expr     AST    AST 
+  | Assignment  Variable Expr
+  | Error       String
   | Empty
   deriving (Show)
 
-types = ["int"]
-condToken = ["if"]
-loopToken = ["while"]
+data Expr 
+  = Sum   Expr Expr
+  | Diff  Expr Expr
+  | Mul   Expr Expr
+  | Div   Expr Expr
+  | Eq    Expr Expr
+  | UnEq  Expr Expr
+  | Grt   Expr Expr 
+  | GrEq  Expr Expr
+  | Lest  Expr Expr 
+  | LesEq Expr Expr 
+  | Not   Expr
+  | Neg   Expr
+  | Val   Int
+  | Var   Variable
+  deriving (Show)
 
------ Exp
+data Variable = Id VarName
+  deriving (Show)
 
-expr :: String -> (AST,String)
-expr xs =
-  let (val,rest) = term xs
-  in termList val rest
+data Declaration = Decl Variable
+  deriving (Show)
 
-termList val ('+':xs) =
-  let (val1,rest) = term xs
-  in termList (Add val val1) rest
-termList val ('-':xs) =
-  let (val1,rest) = term xs
-  in termList (Sub val val1) rest
-termList val xs = (val,xs)
+type VarName = String
 
-term :: String -> (AST,String)
-term xs =
-  let (val,rest) = factor xs
-  in factorList val rest
+-- Handling errors
 
-factorList val ('*':xs) =
-  let (val1,rest) = factor xs
-  in factorList (Mult val val1) rest
-factorList val ('/':xs) =
-  let (val1,rest) = factor xs
-  in factorList (Div val val1) rest
-factorList val xs = (val,xs)
+instance Eq AST where
+  Error _ == Error _ = True
+  Error _ == _       = False
+  _       == Error _ = False
 
-factor :: String -> (AST,String)
-factor ('(':xs) =
-  let (val,rest) = expr xs
-  in if succToken rest ')'
-    then (val,tail rest)
-    else (val,('(':xs))
-factor xs = 
-  if isDigit $ head xs
-    then 
-      let tmpNum = span isDigit xs
-      in (Val (read (fst tmpNum) :: Int), snd tmpNum)
-    else
-      variable xs
 
------ BoolExp
+------------------------
+----- Tokens
+------------------------
 
-boolExpr :: String -> (AST,String)
-boolExpr ('!':xs) =
-  let (val,rest) = expr xs
-  in (Not val,rest)
-boolExpr xs =
-  let (val,rest) = expr xs
-  in boolList val rest
+if_token    = "if"
+else_token  = "else"
+while_token = "while"
+do_token    = "do"
+for_token   = "for"
 
-boolList val ('=':'=':xs) =
-  let (val1,rest) = expr xs
-  in (Eq val val1, rest)
-boolList val ('>':xs) =
-  let (val1,rest) = expr xs
-  in (Gt val val1, rest)
-boolList val ('<':xs) =
-  let (val1,rest) = expr xs
-  in (Lt val val1,rest)
-boolList val xs = expr xs
+------------------------
+----- Support functions
+------------------------
 
------ Assign
-
-assign :: String -> (AST,String)
-assign xs =
-  let (val,rest) = variable xs
-  in if succToken rest '='
-    then let (val1,rest1) = expr $ tail rest
-      in ((Assign val val1), rest1)
-    else (val,rest)
-
-variable :: String -> (AST,String)
-variable xs =
-  let (val,rest) = formatVar xs
-  in if validVar val
-    then (Id val,rest)
-    else (Empty,xs)
-
-formatVar xs = span validChar xs
-
-validVar (x:xs) = x `elem` ['a'..'z'] && and (map validChar xs)
-validVar [] = False
-
-validChar x = x `elem` ['A'..'Z'] || x `elem` ['a'..'z'] || x `elem` ['0'..'9']
-
------ Decl
-
-{-
-PROBLEMA:
-Catena di dichiarazioni.
-Se ho tante dichiarazioni come richiamare la funzione
-'declaration' a "catena"?
-Termina con ';', si confonde con gli altri statement!
-
--}
-
-declaration :: String -> (AST,String)
-declaration xs =
-  let (val,rest) = typeVar xs
-      (val1, rest1) = variable rest
-  in if succToken rest1 ';'
-    then (Decl val val1, tail rest1)
-    else if succToken rest1 '='
-      then let (val2,rest2) = assign rest
-          in (Decl val val2, tail rest2)
-      else (Empty,xs)
-
-typeVar :: String -> (AST,String)
-typeVar xs =
-  let t = filter (checkToken xs) types
-  in if t /= []
-     then (Type $ head t, drop ((length.head) t) xs)
-     else (Empty,xs)
-
-checkToken xs ys = and $ zipWith (==) xs ys
+skipToken :: [Char] -> [Char] -> [Char]
+skipToken xs [] = xs
+skipToken (x:xs) (t:ts) 
+  | x == t    = skipToken xs ts
+  | otherwise = (x:xs)
 
 succToken [] _ = False
 succToken xs t 
   | head xs == t = True
   | otherwise    = False
 
------ Cond
+checkToken xs ys = and $ zipWith (==) xs ys
 
-condition :: String -> (AST,String)
-condition xs =
-  let (val,rest) = takeToken (filter (checkToken xs) condToken) xs
-  in if succToken rest '('
-    then 
-      let (val1,rest1) = boolExpr (tail rest)
-      in if head rest1 == ')'
-        then
-          let (val2,rest2) = compose (tail rest1)
-          in (Cond val1 val2, rest2)
-        else
-          (val1,rest1)
-    else (Empty,xs)
+formatVar xs = 
+  let (var,rest) = span isAlphaNum xs
+  in  if validVar var
+      then  (var,rest)
+      else  ("",xs)
 
-takeToken [] xs = ("",xs)
-takeToken ts xs = (head ts, drop ((length.head) ts) xs)
+validVar (x:xs) = (isAlpha x) && and (map isAlphaNum xs)
+validVar [] = False
 
------ Loop
+--------------------
+----- Parser
+--------------------
 
-loop :: String -> (AST,String)
-loop xs =
-  let (val, rest) = takeToken (filter (checkToken xs) loopToken) xs
-  in if succToken rest '('
-    then 
-      let (val1,rest1) = boolExpr (tail rest)
-      in if succToken rest1 ')'
-        then
-          let (val2,rest2) = compose (tail rest1)
-          in (Loop val1 val2, rest2)
-        else
-          (val1,rest1)
-    else (Empty,xs)
+-- expr
 
------ Block
+expression :: String -> (Expr,String)
+expression [] = (Val 1, "")   --Used in for statement for rapresenting infinite loop (missing condition) 
+expression xs =
+  let (val,rest) = equality xs
+  in (val,rest)
 
-compose :: String -> (AST,String)
-compose ('{':xs) =
+equality :: String -> (Expr,String)
+equality xs =
+  let (val,rest) = comparison xs
+  in  comparisonList val rest
+
+comparisonList val ('=':'=':xs) =
+  let (val1,rest) = comparison xs
+  in comparisonList (Eq val val1) rest
+comparisonList val ('!':'=':xs) =
+  let (val1,rest) = comparison xs
+  in comparisonList (UnEq val val1) rest
+comparisonList val xs = (val,xs)
+
+comparison :: String -> (Expr,String)
+comparison xs =
+  let (val,rest) = term xs
+  in termList val rest
+
+termList val ('>':'=':xs) =
+  let (val1,rest) = term xs
+  in termList (GrEq val val1) rest
+termList val ('>':xs) =
+  let (val1,rest) = term xs
+  in termList (Grt val val1) rest
+termList val ('<':'=':xs) =
+  let (val1,rest) = term xs
+  in termList (LesEq val val1) rest
+termList val ('<':xs) =
+  let (val1,rest) = term xs
+  in termList (Lest val val1) rest
+termList val xs = (val,xs)
+
+term :: String -> (Expr,String)
+term xs =
+  let (val,rest) = factor xs
+  in factorList val rest
+
+factorList val ('+':xs) =
+  let (val1,rest) = factor xs
+  in factorList (Sum val val1) rest
+factorList val ('-':xs) =
+  let (val1,rest) = factor xs
+  in factorList (Diff val val1) rest
+factorList val xs = (val,xs)
+
+factor :: String -> (Expr,String)
+factor xs =
+  let (val,rest) = unary xs
+  in unaryList val rest
+
+unaryList val ('*':xs) =
+  let (val1,rest) = unary xs
+  in unaryList (Mul val val1) rest
+unaryList val ('/':xs) =
+  let (val1,rest) = unary xs
+  in unaryList (Div val val1) rest
+unaryList val xs = (val,xs)
+
+unary :: String -> (Expr,String)
+unary ('!':xs) =
+  let (val,rest) = unary xs
+  in (Not val, rest)
+unary ('-':xs) =
+  let (val,rest) = unary xs
+  in (Neg val, rest)
+unary xs = primary xs
+
+primary :: String -> (Expr,String)
+primary ('(':xs) =
+  let (val,rest) = expression xs
+  in if succToken rest ')'
+    then (val,tail rest)
+    else (val,('(':xs))
+primary (x:xs)
+  | isDigit x = (Val (read (fst tmpNum) :: Int), snd tmpNum)
+  | isAlpha x = (Var valVar, rest)
+  where tmpNum = span isDigit (x:xs)
+        (valVar,rest) = variable (x:xs)
+
+-- variable
+
+variable :: String -> (Variable,String)
+variable xs =
+  let (val,rest) = formatVar xs
+  in (Id val,rest)
+
+-- declaration
+
+declaration :: String -> (Declaration,String)
+declaration xs =
+  let (val,rest) = variable xs
+  in  if succToken rest ';'
+      then (Decl val, tail rest)
+      else (Decl val, (';':rest))
+
+-- assignment
+
+assignmentStmt xs =
+  let (val,rest) = assignment xs
+  in  if succToken rest ';'
+      then (val, tail rest)
+      else (Error "Missing ';'", rest)
+
+assignment :: String -> (AST,String)
+assignment xs =
+  let (val,rest) = variable xs
+  in assignmentSeq val rest
+
+assignmentSeq val ('=':xs) =
+  let (val1,rest) = expression xs
+  in (Assignment val val1, rest)
+assignmentSeq val xs =
+  (Error "Missing '='",xs)
+
+-- if statement
+
+ifStmt :: String -> (AST,String)
+ifStmt ('(':xs) = 
+  let (valCond,rest) = expression xs
+  in  if succToken rest ')'
+      then ifCond valCond $ tail rest
+      else (Error "Missing ')' in if statement", xs)
+
+ifCond valExp xs =
+  let (valStmt,rest) = statement xs
+  in  if checkToken rest else_token
+      then 
+        let (valElse,rest1) = optElse $ skipToken rest else_token
+        in (If valExp valStmt valElse, rest1)
+      else (If valExp valStmt Empty, rest)
+
+optElse xs = statement xs
+
+-- while statement 
+
+whileStmt :: String -> (AST,String)
+whileStmt ('(':xs) = 
+  let (valCond,rest) = expression xs
+  in  if succToken rest ')'
+      then whileBlock valCond $ tail rest
+      else (Error "Missing ')' in while statement", xs)
+
+whileBlock valCond xs =
+  let (valStmt,rest) = statement xs
+  in  (While valCond valStmt, rest)
+
+-- do statement
+
+doStmt :: String -> (AST,String)
+doStmt ('{':xs) =
+  let (valStmt,rest) = statement xs
+  in  if succToken rest '}'
+      then
+        if checkToken (tail rest) while_token
+        then 
+          let (valCond,rest1) = doWhileCondition $ skipToken (tail rest) while_token
+          in  if succToken rest1 ';'
+              then (DoWhile valStmt valCond, tail rest1)
+              else (Error "Missing ';' after while condition",rest1)
+        else (Error "Missing 'while'",rest)
+      else (Error "Missing '}' in do statement",xs)
+doStmt xs = (Error "Missing '{' in do statement",xs)
+
+doWhileCondition ('(':xs) = 
+  let (valCond,rest) = expression xs
+  in  if succToken rest ')'
+      then (valCond, tail rest)
+      else (valCond,(')':xs))
+
+-- for statement
+
+forStmt :: String -> (AST,String)
+forStmt ('(':xs) =
+  let (parFor,rest) = extractPar xs
+  in  if (length parFor) == 3 && succToken rest ')'
+      then 
+        let (blockFor,rest1) = createBlockFor parFor $ tail rest
+        in  (Block [] blockFor,rest1)
+      else
+        (Error "For syntax", rest)
+
+createBlockFor :: [String] -> String -> ([AST],String)   
+createBlockFor (init:cond:step : ps) xs =
+  let (valBody, rest)  = createBodyWhile step xs
+      (initVal, initR) = assignment init
+      (condVal, condR) = expression cond
+  in  if initR == "" && condR == ""
+      then ( initVal : (While condVal valBody) : [], rest )
+      else ([Error "For syntax"], xs)
+
+createBodyWhile :: String -> String -> (AST,String)
+createBodyWhile step xs =
+  let (val,rest) = statement xs
+      (stepVal,_) = assignment step
+  in (Block [] ([val] ++ [stepVal]), rest)
+
+extractPar xs = (\ x -> (splitOn ";" $ fst x, snd x) ) $ break (== ')') xs
+
+-- block
+
+compound :: String -> (AST,String)
+compound [] = (Empty, "")
+compound ('{':xs) =
   let (val,rest) = stmtChain xs
   in if succToken rest '}'
     then (val,tail rest)
-    else (val,rest)
-compose xs = (Empty,xs)
-
------ Stmt
+    else (Error "Something went wrong :/",rest)
 
 stmtChain :: String -> (AST,String)
 stmtChain xs =
-  let (val,rest) = stmt xs
-  in stmtList val rest
+  let (decL,rest) = decChain xs
+  in  if rest /= []
+      then  let (astL,rest1) = astChain rest
+            in (Block decL astL, rest1)
+      else  (Block decL [], rest)
 
-stmtList val (';':'}':xs) =
-  (val,'}':xs) 
-stmtList val (';':xs) =
-  let (val1,rest) = stmt xs
-  in stmtList (Seq val val1) rest
-stmtList val xs = (val,xs)
+decChain :: String -> ([Declaration],String)
+decChain ('i':'n':'t':xs) =
+  let (val,rest) = declaration xs
+  in decList [val] rest
+decChain xs = ([],xs)
 
-stmt :: String -> (AST,String)
-stmt xs 
-  | checkStmt xs condToken = condition xs
-  | checkStmt xs loopToken = loop xs
-  | otherwise              = assign xs
+decList val ('i':'n':'t':xs) = 
+  let (val1,rest) = declaration xs 
+  in decList (val ++ [val1]) rest
+decList val xs = (val,xs)
 
-checkStmt xs ts 
-  | length (filter (checkToken xs) ts) > 0 = True
-  | otherwise                              = False
+astChain :: String -> ([AST],String)
+astChain [] = ([],"")
+astChain xs = astList [] xs
 
------ Program
+astList val [] = (val,"")
+astList val ('}':xs) = (val,'}':xs)
+astList val xs 
+  | val1 == Error "" = ([val1],xs)
+  | otherwise = astList (val ++ [val1]) rest
+  where (val1,rest) = statement xs
 
-program xs =
-  let cleanText = foldr (++) [] $ words xs
-      (dec,rest) = declaration cleanText
-      (prog,rest1) = compose rest
-  in (show dec) ++ " " ++ (show prog)
+statement xs 
+  | checkToken xs if_token    = ifStmt $ skipToken xs if_token
+  | checkToken xs while_token = whileStmt $ skipToken xs while_token
+  | checkToken xs do_token    = doStmt $ skipToken xs do_token
+  | checkToken xs for_token   = forStmt $ skipToken xs for_token
+  | isAlpha $ head xs         = assignmentStmt xs
+  | head xs == '{'            = compound xs
+  | otherwise                 = (Error "Mismatching error",xs)
 
------
+-- Program
+
+program :: String -> (AST,String)
+program xs = compound xs
 
 main :: IO ()
 main = do
   [inputFile,outputFile] <- getArgs
   file <- readFile inputFile
-  let out = show $ compose $ foldr (++) [] $ words file
+  let out = show $ program $ foldr (++) [] $ words file
   putStrLn out
   writeFile outputFile out
